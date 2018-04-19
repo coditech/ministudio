506de0c8394df4ddbd568ff226f760313c1ee826
AnasJamous <anas.jam89@gmail.com>
2018-04-19 15:57:59 +0300

    title has level property

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import './Title.css'
 
-const Title = ({children}) => <h1 className="title">{children}</h1>
+const Title = ({ children, level=1 }) => {
+    if(level === 2) { return  (<h2 className="title">{children}</h2>) }
+    if(level === 3) { return  (<h3 className="title">{children}</h3>) }
+    if(level === 4) { return  (<h4 className="title">{children}</h4>) }
+    if(level === 5) { return  (<h5 className="title">{children}</h5>) }
+    if(level === 6) { return  (<h6 className="title">{children}</h6>) }
+    return  (<h1 className="title">{children}</h1>)
+}
 
 export default Title
