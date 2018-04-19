c1ff02795ae74ecefe8180afb635f584132fdc22
AnasJamous <anas.jam89@gmail.com>
2018-04-19 16:39:19 +0300

    titles takes classnames now

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Title.css'
 
-const Title = ({ children, level=1 }) => {
-    if(level === 2) { return  (<h2 className="title">{children}</h2>) }
-    if(level === 3) { return  (<h3 className="title">{children}</h3>) }
-    if(level === 4) { return  (<h4 className="title">{children}</h4>) }
-    if(level === 5) { return  (<h5 className="title">{children}</h5>) }
-    if(level === 6) { return  (<h6 className="title">{children}</h6>) }
-    return  (<h1 className="title">{children}</h1>)
+const Title = ({ children, level=1, className }) => {
+    className = className ? 'title '+className : 'title'
+    if(level === 2) { return  (<h2 className={className}>{children}</h2>) }
+    if(level === 3) { return  (<h3 className={className}>{children}</h3>) }
+    if(level === 4) { return  (<h4 className={className}>{children}</h4>) }
+    if(level === 5) { return  (<h5 className={className}>{children}</h5>) }
+    if(level === 6) { return  (<h6 className={className}>{children}</h6>) }
+    return  (<h1 className={className}>{children}</h1>)
 }
 
 export default Title
