114d3a560e6719005ee0f4b25153de1b366dd450
Aliosseily <ali.ossaily@outlook.com>
2018-04-19 14:36:05 +0300

    video section test

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import Chat from "./Chat";
+import './App.css'
 
-class Video extends Component{
-    render(){
-        return(
-            <div>Video</div>
-        )
+class Video extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            videoURL: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4'
+        }
     }
-}
-export default Video
+    
+    Relaod = () =>{
+      window.location="https://www.youtube.com/channel/UCOwyzWourCke41irmVcEtCQ/videos"
+    }
+    render () {
+        return (
+          <div>
+            <video className="background-video" loop autoPlay muted >
+                <source src={this.state.videoURL} type="video/mp4" />
+               </video>
+                  <div className = "divbtn" onClick = {this.Relaod}>Watch More</div>
+                     <div>
+                         </div>
+                              </div>
+                              )
+                            }
+                          }; 
+export default Video;
+
