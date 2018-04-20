20915e09fc05af5e45a0c233f9177d9f5b54c111
salehsaadi <salehsaadi.91@gmail.com>
2018-04-20 15:15:09 +0300

    navbar added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "../../node_modules/picnic/picnic.min.css";
 import "./Navbar.css";
+import DropDown from './DropDown'
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div className="nav"> 
-            <img className="logo" src={require("../logo.png")} />
-             <nav className="navbar">
-            
-              {/* responsive */}
-              <input id="bmenub" type="checkbox" className="show" />
-              <label for="bmenub" className="burger pseudo button">&#8801;</label>
-            
-              <div class="menu">
-                <a href="#" className="new pseudo icon-picture">home</a>
-                <select>
-                    <option><a href="#" className="new pseudo icon-picture">services</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">Video Production</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">BIRTHDAYS</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">EVENTS</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">Live Shows</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">Summer Camp</a></option>
-                </select>
-                <select>
-                    <option><a href="#" className="new pseudo icon-picture">Videos</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">DIY</a></option>
-                    <option><a href="#" className="new pseudo icon-picture">ZUMBA</a></option>
-                </select>
-                <a href="#" className="new pseudo icon-picture">store</a>
-                <a href="#" className="new pseudo icon-picture">about</a>
-                <a href="#" className="new pseudo icon-picture">contact</a>
-                <a href="#" className="new pseudo icon-picture">blog</a>
+const Navbar = () => (
+    <div className="nav"> 
+        <img className="logo" alt="Ministudio logo" src={require("../logo.png")} />
+        <nav className="navbar">
+        
+        {/* responsive */}
+        <input id="bmenub" type="checkbox" className="show" />
+        <label htmlFor="bmenub" className="burger pseudo button">&#8801;</label>
 
-              </div>
-            </nav>
-            
-            </div>
-        )
-    }
-}
+        <div className="menu">
+            <span href="#" className="new pseudo icon-picture">home</span>
+            <DropDown title="services">
+                <a className="pseudo icon-picture">Video Production</a>
+                <a className="pseudo icon-picture">BIRTHDAYS</a>
+                <a className="pseudo icon-picture">EVENTS</a>
+                <a className="pseudo icon-picture">Live Shows</a>
+                <a className="pseudo icon-picture">Summer Camp</a>
+            </DropDown>
+           
+            <DropDown title="videos">
+                <a className="pseudo icon-picture">DIY</a>
+                <a className="pseudo icon-picture">ZUMBA</a>
+            </DropDown>
+            <a href="#" className="new pseudo icon-picture">store</a>
+            <a href="#" className="new pseudo icon-picture">about</a>
+            <a href="#" className="new pseudo icon-picture">contact</a>
+            <a href="#" className="new pseudo icon-picture">blog</a>
+
+        </div>
+    </nav>
+    
+    </div>
+)
+
 export default Navbar;
